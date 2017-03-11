a9458f16c3dd31659c61fdd2867fe5dc1516ab55
Victor Gama <hey@vito.io>
2017-03-11 14:32:12 -0300

    Update tests

--- spec/index.js
+++ spec/index.js
@@ -4,16 +4,16 @@ var unfurl = require('../lib/unfurl');
 describe('unfurl-url', function() {
     var subjects = {
         'bitly': {
-            'http://j.mp/Y4seGv': 'http://www.nytimes.com/2013/03/11/world/asia/karzai-accuses-us-and-taliban-of-colluding-in-afghanistan.html?ref=global-home&_r=0',
+            'http://j.mp/Y4seGv': 'http://www.nytimes.com/2013/03/11/world/asia/karzai-accuses-us-and-taliban-of-colluding-in-afghanistan.html?ref=global-home',
             'http://bit.ly/1T1Hul6': 'http://www.polygon.com/2015/11/18/9757114/fallout-4-vault-tec-phone-call'
         },
         't.co': {
-            'http://t.co/bxPFQgZ1AV': 'http://www.nytimes.com/2013/03/14/crosswords/bridge/bridge-spring-north-american-championships.html?partner=rss&emc=rss&_r=0',
+            'http://t.co/bxPFQgZ1AV': 'http://www.nytimes.com/2013/03/14/crosswords/bridge/bridge-spring-north-american-championships.html?partner=rss&emc=rss',
             'https://t.co/SlINea0uWD': 'https://github.com/lyst/MakingLyst/blob/master/mobile/ios/coding-standards/style-guide.md'
         },
         'arrows': {
             'http://➡.ws/kd': 'http://www.theglobeandmail.com/technology/tech-news/crtc-will-rescind-unlimited-use-internet-decision---or-ottawa-will-overturn-it/article565223/',
-            'http://➡.ws/wwwwwwwww': 'http://expandurl.appspot.com/'
+            'http://➡.ws/wwwwwwwww': 'http://www.linkexpander.com/'
         },
         'tinyurl': {
             'http://tinyurl.com/pj94dvk': 'https://github.com/blog/2085-a-new-look-for-repositories',
